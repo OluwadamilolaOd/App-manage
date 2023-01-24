1cb6174b5353b557f04a44fc58d7dfaa367c8c40
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-01-24 10:42:35 +0100

    initialize repo

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Dashboard.css'
+
+function Dashboard() {
+  return (
+    <div className='Dashboard'>
+        <div className="navBar"></div>
+        <div className="body">
+            <div className="sideBar"></div>
+            <div className="otherBar"></div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Dashboard
