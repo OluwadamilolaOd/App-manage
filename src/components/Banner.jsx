55643acbcf0277b9a05cec316d0d394577b4244a
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-05-02 11:08:47 +0100

    Added header component

---
+++ src/components/Banner.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="banner">
+        <div className="wrapper">
+            <h1>Manage Organisation</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Styles/header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+        <div className="header_right">
+            <p>Joel Badmus </p> <br />
+            <span>Ha-Shem Limited</span>
+          <div className="profile">
+            
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Header
