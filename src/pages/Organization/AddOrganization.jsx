25e3d2ca2ae63b6ad69167a5eae5458a1ae7bf7c
Azeem Olatunji <Azeemol@HA-SHEM.com>
2023-05-03 10:52:03 +0100

    Added add organization

--- src/pages/Organization/AddOrganization.jsx
+++ src/pages/Organization/AddOrganization.jsx
@@ -1,9 +1,105 @@
-import React from 'react'
+import React, { useState } from "react";
+import "../../components/Styles/addorganization.css";
 
 const AddOrganization = () => {
-  return (
-    <div>AddOrganization</div>
-  )
-}
+  const [companyName, setCompanyName] = useState("");
+  const [emailAddress, setEmailAddress] = useState("");
+  const [location, setLocation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
 
-export default AddOrganization
+  return (
+    <div>
+      <form className="form_container">
+        <div className="title-head"><h3>Organization Details</h3></div>
+        <div className="forminput">
+          <div className="section">
+            <div>
+              <label htmlFor="company-name">Company Name:</label>
+              <input
+                type="text"
+                id="company-name"
+                value={companyName}
+                onChange={(event) => setCompanyName(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email-address">Email Address:</label>
+              <input
+                type="email"
+                id="email-address"
+                value={emailAddress}
+                onChange={(event) => setEmailAddress(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className="section">
+            <div>
+              <label htmlFor="location">Location:</label>
+              <input
+                type="text"
+                id="location"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone-number">Phone Number:</label>
+              <input
+                type="tel"
+                id="phone-number"
+                value={phoneNumber}
+                onChange={(event) => setPhoneNumber(event.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="title-head"><h3>License Details</h3></div>
+        <div className="forminput">
+          <div className="section">
+            <div>
+              <label htmlFor="company-name">License Name:</label>
+              <input
+                type="text"
+                id="company-name"
+                value={companyName}
+                onChange={(event) => setCompanyName(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="email-address">Start Date:</label>
+              <input
+                type="email"
+                id="email-address"
+                value={emailAddress}
+                onChange={(event) => setEmailAddress(event.target.value)}
+              />
+            </div>
+          </div>
+          <div className="section">
+            <div>
+              <label htmlFor="location">Band Type:</label>
+              <input
+                type="text"
+                id="location"
+                value={location}
+                onChange={(event) => setLocation(event.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="phone-number">Expiration Date:</label>
+              <input
+                type="tel"
+                id="phone-number"
+                value={phoneNumber}
+                onChange={(event) => setPhoneNumber(event.target.value)}
+              />
+            </div>
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddOrganization;
