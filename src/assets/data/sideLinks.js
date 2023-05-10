cdad43fc348e521e57dd4a4df265a4c868fae68a
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-05-10 09:45:11 +0100

    iconwhite

--- src/assets/data/sideLinks.js
+++ src/assets/data/sideLinks.js
@@ -1,20 +1,27 @@
 import home from '../images/home-black.svg';
 import license from '../images/license-black.svg'
 import organization from '../images/organization-black.svg'
+import homeW from '../images/home-white.svg'
+import licenseW from '../images/license-white.svg'
+import organizationW from '../images/organization-white.svg'
+
 export const sideLinks = [
     {
         path: "/home",
         icon: home,
+        iconW: homeW,
         display: "Home",
     },
     {
         path: "/license",
         icon: license,
+        iconW: licenseW,
         display: "License",
     },
     {
         path: "/organizations",
         icon: organization,
+        iconW: organizationW,
         display: "Organizations",
     }
 
