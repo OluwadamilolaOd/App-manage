0507b7e37408bc197f56b3eecfc51eadd93a429a
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-06-30 09:44:48 +0100

    download

--- src/pages/ActionPage/Download/DownloadModal.jsx
+++ src/pages/ActionPage/Download/DownloadModal.jsx
@@ -1,19 +1,15 @@
-import { useState } from 'react';
-import imageIcon from '../../../assets/images/download_blue.png'
+import { useState } from "react";
+import imageIcon from "../../../assets/images/download_blue.png";
 import { CSVLink } from "react-csv";
 // import { PDFDownloadLink, Document, Page, Text } from 'react-pdf';
 
-
-
-function DownloadModal({ setOpenModal,data }) {
-
+function DownloadModal({ setOpenModal, data }) {
   // const [csvData, setCsvData] = useState([]);
   // const handlePDF = (e) => {
   //   e.preventDefault()
   //   console.log(data)
   //   console.log("helloooPDF")
   // }
-
 
   // const MyPdfDocument = () => (
   //   <Document>
@@ -23,37 +19,38 @@ function DownloadModal({ setOpenModal,data }) {
   //   </Document>
   // );
 
-  
-
   const entries = Object.entries(data);
 
   return (
     <div className="modal">
-    <div onClick={()=>{setOpenModal(false)}} className="overlay"></div>
-    <div className="modal-content">
-      <div className='modalTitle downloadModalTile'><h2>Export License</h2></div>     
-      <div className="logoutmodalwrapper">
-      <img className='modalImg' src={imageIcon} alt='Error'></img>
-      <h4>{data.organizationName}-{data.licenseName}</h4>  
-      <p id='downloadP'>
-        Export License Information
-      </p>
-      </div>
-      <div className="modalbtnwrapper">
-      {/* <div className="modalButton ">
-        <button className="downloadModalButton" onClick={handlePDF}>
-        PDF
-      </button>
-      </div> */}
-      <div className="modalButton ">
-        <CSVLink className='btnblue' data={entries} filename={`${data.organizationName}-license-file.csv`}>Download me</CSVLink>
-{/* <button className="downloadModalButton" onClick={handleCsv}>
-        EXCEL
-      </button> */}
-      </div>
+      <div
+        onClick={() => {
+          setOpenModal(false);
+        }}
+        className="overlay"
+      ></div>
+      <div className="modal-content">
+        <div className="modalTitle downloadModalTile">
+          <h2>Export License</h2>
+        </div>
+        <div className="logoutmodalwrapper">
+          <img className="modalImg" src={imageIcon} alt="Error"></img>
+          <h4>
+            {data.organizationName}-{data.licenseName}
+          </h4>
+          <p id="downloadP">Export License Information</p>
+        </div>
+        <div className="modalButton">
+          <CSVLink
+            className="btnblue"
+            data={entries}
+            filename={`${data.organizationName}-license-file.csv`}
+          >
+            Download me
+          </CSVLink>
+        </div>
       </div>
     </div>
-  </div>
   );
 }
 
