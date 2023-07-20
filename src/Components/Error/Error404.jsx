5b3dd7856d214636e66710e099a0cd01a8606c91
Oluwadamilola Odujirin <oluwadamilolaod@HA-SHEM.com>
2023-07-20 12:29:54 +0100

    errorPage

--- src/Components/Error/Error404.jsx
+++ src/Components/Error/Error404.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from "react";
+import "./error.css";
+import Error404_Picture from "../../assets/images/error404.png";
+import { BiError } from "react-icons/bi";
 
 const Error404 = () => {
   return (
-    <div className='landingPage'>
-      <div className="error-page">
-        <div>A</div>
-        <div>B</div>
+    <div className="errorPage">
+      <div className="errorSection">
+        <div className="error">
+          <img src={Error404_Picture} alt="" className="picture" />
+          <div className="text-error">
+            <BiError className="error-icon" />
+            <h1 className="bigtxt-error">Sorry! Something went wrong</h1>
+            <p className="smalltxt-error">
+              Lorem ipsum dolor sit amet, conse ctetur adipiscing elit. Luctus
+              vel arcu, tellus tincidunt at sagittis cursus.
+            </p>
+            <div className="btn-page">
+              <button className="btn">Try again</button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Error404
+export default Error404;
