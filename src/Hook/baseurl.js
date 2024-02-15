95358bd38c590ffb711214eb25da3d6c19686846
O-AbdulAzeem <azeemol@ha-shem.com>
2024-02-15 14:58:07 +0100

    update the env file

--- src/Hook/baseurl.js
+++ src/Hook/baseurl.js
@@ -1,5 +1,5 @@
-//export const baseUrl = "https://localhost:7103/api"
+export const baseUrl = "https://localhost:7103/api"
 //export const baseUrl = "https://unifiedlicenseapi.azurewebsites.net"
 //export const baseUrl = "https://unifiedlicense20230601104842.azurewebsites.net/api"
 //export const baseUrl = "https://unifiedlicenseportal20230724115858.azurewebsites.net/api"
-export const baseUrl = "https://appmanageapi.azurewebsites.net/api"
+//export const baseUrl = "https://appmanageapi.azurewebsites.net/api"
